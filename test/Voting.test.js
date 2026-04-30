479191dd9a88e85adb8d0374488f6be48a22dc53
kkhanh-kas <kkhanhnnuyen@gmail.com>
2026-04-30 17:18:49 +0700

    more note

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -1,3 +1,9 @@
 /**
- * Chứa 6 test cases tự động 
+ * Chứa 6 test cases tự động như sau: 
+ * Contract khởi tạo đúng số ứng viên
+ * Thông tin ứng viên ban đầu đúng (voteCount = 0)
+ * Vote thành công → voteCount tăng lên 1
+ * Từ chối vote cho ứng viên id không tồn tại
+ * Từ chối vote 2 lần từ cùng địa chỉ
+ * Event votedEvent được phát đúng
  */
