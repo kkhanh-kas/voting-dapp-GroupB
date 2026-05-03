f8f309ef71721d9b7104e09907c70f4e267782c1
milaneee205-glitch <milaneee205@gmail.com>
2026-05-04 05:45:01 +0700

    layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
+ return (
     <div className="min-h-screen bg-gray-50">
 
       {/* HEADER */}
